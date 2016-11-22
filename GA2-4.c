d7444899d1751cd111e3049b76cdd690adc450ac
Neck Luo <luo4neck@gmail.com>
2016-11-22 15:40:14 +0000

    addition format change

--- GA2-4.c
+++ GA2-4.c
@@ -209,80 +209,82 @@ void free_GA(GRP* p)//doing memory free..
 
 int main(int argc, char *argv[])
 {
-GRP* p;
-int i,j,itr,g,n,A,B,tmp,son,mut_p;
-double cross_r, mut_r,R,ra=0.5,rb=0.5;
-/*-----------------checking the input values----------------*/
-if( argv[6]!= NULL || argv[1]==NULL || argv[2]==NULL || argv[3]==NULL || argv[4]==NULL || argv[5]==NULL)
-  {
-  printf("Please input command in this way:\nGA 10(population size) 10(number of generations) ");
-  printf("100(number of iterations) 0.95(crossover rate) 0.1(mutation rate)\n");
-  exit(0);
-  }
-n = atoi(argv[1]);
-g = atoi(argv[2]);
-itr = atoi(argv[3]);
-cross_r = atof(argv[4]);
-mut_r = atof(argv[5]);
-if (cross_r>1.0 || mut_r>1.0 || mut_r>cross_r)
-  {
-  printf("Crossover rate must larger than mutation rate and both not larger than 1.\n");
-  exit(0);
-  }
-/*-----------------checking the input values----------------*/
-
-p = alloc_GA(n);
-srand48(time(NULL));
-
-for(i=0;i<itr;i++)
-  {
-  printf("\nIn iteration %d:\n\n",i+1);
-
-  for(j=0;j<n*2;j++)//randomly sorting the rank of the team to set partenrs for next robbery..
-  {
-  A=lrand48()%n;
-  B=lrand48()%n;
-  tmp = p->position[A];
-  p->position[A] = p->position[B];
-  p->position[B] = tmp;
-  }
-
-  for(j=0;j<n/2;j++)//each of robber should face PD once in one iteration..
+    GRP* p;
+    int i,j,itr,g,n,A,B,tmp,son,mut_p;
+    double cross_r, mut_r,R,ra=0.5,rb=0.5;
+    /*-----------------checking the input values----------------*/
+    if( argv[6]!= NULL || argv[1]==NULL || argv[2]==NULL || argv[3]==NULL || argv[4]==NULL || argv[5]==NULL)
     {
-    A = p->position[2*j];
-    B = p->position[2*j+1];
-    ra = drand48();
-    rb = drand48();
-    decide_GA(p,ra,rb,A,B);
+        printf("Please input command in this way:\nGA 10(population size) 10(number of generations) ");
+        printf("100(number of iterations) 0.95(crossover rate) 0.1(mutation rate)\n");
+        exit(0);
     }
+
+    n = atoi(argv[1]);
+    g = atoi(argv[2]);
+    itr = atoi(argv[3]);
+    cross_r = atof(argv[4]);
+    mut_r = atof(argv[5]);
+    if (cross_r>1.0 || mut_r>1.0 || mut_r>cross_r)
+    {
+        printf("Crossover rate must larger than mutation rate and both not larger than 1.\n");
+        exit(0);
+    }
+/*-----------------checking the input values----------------*/
+
+    p = alloc_GA(n);
+    srand48(time(NULL));
+
+    for(i=0;i<itr;i++)
+    {
+        printf("\nIn iteration %d:\n\n",i+1);
+
+        for(j=0;j<n*2;j++)//randomly sorting the rank of the team to set partenrs for next robbery..
+        {
+            A=lrand48()%n;
+            B=lrand48()%n;
+            tmp = p->position[A];
+            p->position[A] = p->position[B];
+            p->position[B] = tmp;
+        }
+
+        for(j=0;j<n/2;j++)//each of robber should face PD once in one iteration..
+        {
+            A = p->position[2*j];
+            B = p->position[2*j+1];
+            ra = drand48();
+            rb = drand48();
+            decide_GA(p,ra,rb,A,B);
+        }
   
-  for(j=0;j<g;j++)//g generations happened in one iteration..
-    {
-    printf("Generation %d:\n",j+1);
-    A = lrand48()%n;
-    B = lrand48()%n;
-    while(B==A)
-    {B = lrand48()%n;}
+        for(j=0;j<g;j++)//g generations happened in one iteration..
+        {
+            printf("Generation %d:\n",j+1);
+            A = lrand48()%n;
+            B = lrand48()%n;
+            while(B==A)
+            {B = lrand48()%n;}
     
-    R = drand48();//for judgement of crossover and mutation..
+            R = drand48();//for judgement of crossover and mutation..
     
-    if(R<cross_r)//judge if crossover will happen..
-      {
-      son = lrand48()%n;
-      mut_p = lrand48()%4;// this value tells the position of mutation happen..
-      cross_GA(p,A,B,son,R,mut_p,mut_r);
-      print_GA(p,n);
-      }
-    else printf("No crossover and mutation happened.\n");
+            if(R<cross_r)//judge if crossover will happen..
+            {
+                son = lrand48()%n;
+                mut_p = lrand48()%4;// this value tells the position of mutation happen..
+                cross_GA(p,A,B,son,R,mut_p,mut_r);
+                print_GA(p,n);
+            }
+            else 
+                printf("No crossover and mutation happened.\n");
+        }
+
+        for(j=0;j<n;j++)//fitness function..
+        {
+            fitness_GA(p,j,p->his[j]);
+        }
+        //  print_GA(p,n);
     }
 
-  for(j=0;j<n;j++)//fitness function..
-    {
-    fitness_GA(p,j,p->his[j]);
-    }
-//  print_GA(p,n);
-  }
-
-free_GA(p);
-return 0;
+    free_GA(p);
+    return 0;
 }
